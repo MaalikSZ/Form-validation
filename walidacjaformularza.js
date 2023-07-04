a81c5e1f886d14ea0f1bc5dbac1a651b73c88b19
Szymon Wasik (Malik) <szymon.wasik890@gmail.com>
2023-07-04 16:38:52 +0200

    Add files via upload

---
+++ walidacjaformularza.js
@@ -0,0 +1,24 @@
+﻿function validateForm() {
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var phone = document.getElementById("phone").value;
+  
+    var nameRegex = /^[a-zA-ZęóąśłżźćńĘÓĄŚŁŻŹĆŃ\s]+$/;
+    var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    var phoneRegex = /^(\+?48)?[ -]?(\d{3}[ -]?){2}\d{3}$/;
+  
+    if (!nameRegex.test(name)) {
+      alert("Proszę podać prawidłowe imię.");
+      return false;
+    }
+  
+    if (!emailRegex.test(email)) {
+      alert("Proszę podać prawidłowy adres e-mail.");
+      return false;
+    }
+  
+    if (!phoneRegex.test(phone)) {
+      alert("Proszę podać prawidłowy numer telefonu.");
+      return false;
+    }
+  }  
